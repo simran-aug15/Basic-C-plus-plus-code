c0e62a44ebc1774783a097686135ff55fc732d6f
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-11-19 23:52:21 +0530

    Added file using float.cpp

---
+++ using float.cpp
@@ -0,0 +1,12 @@
+//using float
+#include <iostream>
+using namespace std;
+int main()
+{
+    float area;
+    int b = 3;
+    int h = 7;
+    area = 0.5 * h * b;
+    cout << "area of triangle :" << area;
+    return 0;
+}
