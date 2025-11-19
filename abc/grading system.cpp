b4f1bece98c94007d2a202291b1fe71eee575b0d
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-11-19 23:52:24 +0530

    Added file grading system.cpp

---
+++ abc/grading system.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int marks;
+   cout << "marks";
+   cin >> marks;
+   if (marks >= 90)
+   {
+      cout << "grade A" << endl;
+   }
+   else if (marks >= 80)
+   {
+      cout << "grade B";
+   }
+   else if (marks >= 70)
+   {
+      cout << "grade C";
+   }
+   else
+   {
+      cout << "default";
+   }
+
+   return 0;
+}
