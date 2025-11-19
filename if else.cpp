238ce86f3ea35920ab2c77afa35e2de6f1d885cc
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-11-19 23:52:18 +0530

    Added file if else.cpp

---
+++ if else.cpp
@@ -0,0 +1,18 @@
+// using if else
+#include <iostream>
+using namespace std;
+int main()
+{
+  int age;
+  age = 20;
+  if (age >= 18)
+  {
+    cout << "can vote" << endl;
+  }
+  else
+  {
+    cout << "can not vote";
+  }
+
+  return 0;
+}
