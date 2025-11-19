8ddc36be7b71be53e9575ce7dac9415b1e1b851f
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-11-19 23:52:23 +0530

    Added file dowhile.cpp

---
+++ abc/dowhile.cpp
@@ -0,0 +1,13 @@
+//using do while
+#include<iostream>
+using namespace std;
+int main()
+{ int index=0;
+    do
+    {
+        cout<<" enter the index:"<<index<<endl;
+        index++;
+    }
+    while(index<34);
+    return 0;
+}
