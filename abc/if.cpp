9bddea19c2e5a2f7f051e9191c171d1639eee070
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-11-19 23:52:25 +0530

    Added file if.cpp

---
+++ abc/if.cpp
@@ -0,0 +1,16 @@
+//using if conditional statement
+#include <iostream>
+using namespace std;
+int main()
+{
+   int age;
+   cout << "age";
+   cin >> age;
+   if (age >= 18)
+   {
+      cout << "license given" << endl;
+   }
+   cout << "license not given";
+
+   return 0;
+}
