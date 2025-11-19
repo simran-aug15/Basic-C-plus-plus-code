0b5ccf8744be60b0caac6aaff41bfde333f4e559
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-11-19 23:52:25 +0530

    Added file license.cpp

---
+++ abc/license.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int age;
+   cout << "age";
+   cin >> age;
+   if (age >= 18)
+   {
+      cout << "license given" << endl;
+   }
+   cout << "license not given";
+
+   return 0;
+}
