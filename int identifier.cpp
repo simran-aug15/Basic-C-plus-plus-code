1cbd98dec17de2031d754912e2aa6a90deaeb779
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-11-19 23:52:18 +0530

    Added file int identifier.cpp

---
+++ int identifier.cpp
@@ -0,0 +1,12 @@
+// indentifier use int
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 3;
+    int b = 6;
+    int sum;
+    sum = a + b;
+    cout << "sum is:" << sum;
+    return 0;
+}
