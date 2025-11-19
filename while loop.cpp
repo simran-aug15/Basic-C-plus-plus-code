ee5e4c89b0c5733ccd2eb820482e50099b5de0c2
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-11-19 23:52:22 +0530

    Added file while loop.cpp

---
+++ while loop.cpp
@@ -0,0 +1,13 @@
+// while loop
+#include <iostream>
+using namespace std;
+int main()
+{
+    int index = 0;
+    while (index < 34)
+    {
+        cout << " enter the index:" << index << endl;
+        index++;
+    }
+    return 0;
+}
