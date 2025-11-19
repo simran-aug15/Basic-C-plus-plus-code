328ec364d56035015dce67159b6bac113f314e1d
simran-aug15 <24011010171@rayatbahrauniversity.edu.in>
2025-11-19 23:52:21 +0530

    Added file switch.cpp

---
+++ switch.cpp
@@ -0,0 +1,38 @@
+//using switch case
+#include <iostream>
+using namespace std;
+int main()
+{
+    int week;
+    cout<<"enter the week:";
+    cin>>week;
+    switch (week)
+    {
+    case 1:
+      {  cout << "monday";
+        break;}
+    case 2:
+        {cout << "tuesday";
+        break;}
+    case 3:
+        {cout << "wednesday";
+        break;}
+    case 4:
+       { cout << "thursday";
+        break;}
+    case 5:
+       { cout << "friday";
+        break;}
+    case 6:
+        {cout << "saturday";
+        break;}
+    case 7:
+        {cout << "sunday";
+        break;}
+        
+    }
+
+
+
+return 0;
+}
